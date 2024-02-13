01f1288e5dcf17ba21f0b07960b6fcacbae9f389
Brenda <brentorres26@gmail.com>
2024-02-12 23:53:40 -0300

    Integración wallet adapter

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@Angular/platform-browser/animations/async';
 import { appRoutes } from './app.routes';
+import { provideWalletAdapter } from '@heavy-duty/wallet-adapter';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes)],
+  providers: [provideRouter(appRoutes), 
+    provideAnimationsAsync(), 
+    provideWalletAdapter()],
 };
