2ad3910779d0a86cda7d10a2ec574104896dd406
Brenda <brentorres26@gmail.com>
2024-02-13 15:39:18 -0300

    configuración de rutas

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,15 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { appRoutes } from './app.routes';
-import { provideWalletAdapter } from '@heavy-duty/wallet-adapter';
 import { provideHttpClient } from '@angular/common/http';
+import { ApplicationConfig } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideRouter } from '@angular/router';
+import { provideWalletAdapter } from '@heavy-duty/wallet-adapter';
+import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes), 
+  providers: [
+    provideRouter(appRoutes), 
     provideAnimationsAsync(), 
     provideWalletAdapter(),
-    provideHttpClient()
+    provideHttpClient(), provideAnimationsAsync()
   ],
 };
