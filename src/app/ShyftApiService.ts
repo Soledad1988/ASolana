70dd4b740149c50c5d2e0f4a14361b112872d3a3
Brenda <brentorres26@gmail.com>
2024-02-14 23:26:54 -0300

    Se agregó método getTransaction para mostrar el historial de transacciones

--- src/app/ShyftApiService.ts
+++ src/app/ShyftApiService.ts
@@ -7,16 +7,16 @@ export class ShyftApiService{
 
     private readonly _httpClient = inject(HttpClient);
     private readonly _header ={ 'x-api-key':'QRCBAfA43rrbM3hC'}; 
-    private readonly _mint = 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v';
+    private readonly _mint = '7EYnhQoR9YM3N7UoaKRoA44Uy8JeaZV3qyouov87awMs';
     
     getAccount(publicKey: string | undefined | null) {
         if (!publicKey) {
             return of(null);
         }
 
-        const url = new URL('https://api.shyft.to/sol/v1/wallet/balance');
+        const url = new URL('https://api.shyft.to/sol/v1/wallet/token_balance');
 
-        url.searchParams.set('network', 'devnet');
+        url.searchParams.set('network', 'mainnet-beta');
         url.searchParams.set('wallet', publicKey);
         url.searchParams.set('token', this._mint);
 
@@ -27,4 +27,23 @@ export class ShyftApiService{
         )
         .pipe(map((response)=> response.result));
     }
+
+    getTransactions(publicKey: string | undefined | null) {
+        if (!publicKey) {
+            return of(null);
+        }
+    
+        const url = new URL('https://api.shyft.to/sol/v1/transaction/history');
+    
+        url.searchParams.set('network', 'mainnet-beta'); 
+        url.searchParams.set('account', publicKey);
+        url.searchParams.set('tx_num', '5');
+      
+        return this._httpClient.get<{ 
+            result: { transactions: string }; 
+        }>(url.toString(), { headers: this._header })
+        .pipe(map(response => response.result));
+    }
+
+
 }
