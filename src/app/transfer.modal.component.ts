0af997656ad99796d77cfa12fbcd03f29216af4b
Brenda <brentorres26@gmail.com>
2024-02-21 10:20:57 -0300

    Se agregó modal y form para transferencia

--- src/app/transfer.modal.component.ts
+++ src/app/transfer.modal.component.ts
@@ -1,15 +1,22 @@
 import { Component } from "@angular/core";
+import { TransferFormComponent, TransferFormPayLoad } from "./transfer-form.component";
 
 
 @Component({
     selector: 'my-proyect-transfer-section',
     standalone: true,
     template: `
-    <div class="px-8 py-16">
-      <h2 class="text-3xl">Transferir Fondos</h2>
+    <div class="px-8 pt-16 pb8-">
+      <h2 class="text-3xl text-center mb-8">Transferir Fondos</h2>
+
+      <my-proyect-transfer-form (submitForm)="onTransfer($event)"></my-proyect-transfer-form>
     </div>
-    `
-  })
+    `,
+    imports: [TransferModalComponent, TransferFormComponent]
+})
 
   export class TransferModalComponent {
+    onTransfer(payload: TransferFormPayLoad){
+      console.log('Hola mundo', payload);
+    }
   }
