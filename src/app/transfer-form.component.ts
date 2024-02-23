aac063b8c040bb8ace17426e37881802aa113d34
Brenda <brentorres26@gmail.com>
2024-02-23 08:47:10 -0300

    se crea formulario para capturar conceptos necesarios para realizar la transferencia

--- src/app/transfer-form.component.ts
+++ src/app/transfer-form.component.ts
@@ -21,86 +21,62 @@ export interface TransferFormPayLoad{
     selector: 'my-proyect-transfer-form',
     standalone: true,
     template: `
-    <form #form="ngForm" class="w-[400px]" (ngSubmit)="onSubmitForm(form)">
+    <form #form="ngForm" (ngSubmit)="onSubmitForm(form)">
+
       <mat-form-field appearance="fill" class="w-full mb-4">
-      <mat-label>Concepto</mat-label>
-      <input 
-      name="memo"
-      matInput 
-      type="text"
-      placeholder="Ejemplo"
-      [(ngModel)]="model.memo"
-      required
-      #memoControl="ngModel"
-      />
-     <mat-icon matSuffix>description</mat-icon>
+          <mat-label>Concepto</mat-label>
+          <input name="memo" matInput type="text" placeholder="Ejemplo"
+            [(ngModel)]="model.memo" required #memoControl="ngModel">
+          <mat-icon matSuffix>description</mat-icon>
 
-      @if(form.submitted && memoControl.errors){
-          <mat-error>
-          @if(memoControl.errors['required']) {
-            El motivo es obligatorio. 
-          }
-          </mat-error> 
-      }@else{
-          <mat-hint>Debe ser el motivo de la transferencia</mat-hint>
-      }
-      <mat-hint>Debe ser el motivo de la transferencia</mat-hint>
-  </mat-form-field>
-
-
-  <mat-form-field appearance="fill" class="w-full mb-4">
-      <mat-label>Monto</mat-label>
-      <input 
-      name="amount"
-      matInput 
-      type="number"
-      min="0"
-      placeholder="Ingrese el monto aquí."
-      [(ngModel)]="model.amount"
-      required
-      #amountControl="ngModel"
-      />
-      <mat-icon matSuffix>attach_money</mat-icon>
-
-      @if(form.submitted && amountControl.errors){
-        <mat-error>
-        @if(amountControl.errors['required']) {
-          El monto es obligatorio.
-          } @else if (amountControl.errors['min']) {
-            El monto debe ser mayor a cero.
-          }
-        </mat-error>
+          @if(form.submitted && memoControl.errors){
+              <mat-error>
+              @if(memoControl.errors['required']) {
+                El motivo es obligatorio. 
+              }
+              </mat-error> 
           }@else{
-          <mat-hint>Debe ser el motivo de la transferencia</mat-hint>
-      }
-      <mat-hint>Debe ser un monto mayor a cero.</mat-hint>
-  </mat-form-field>
-
-
-  <mat-form-field appearance="fill" class="w-full mb-4">
-      <mat-label>Destinatario</mat-label>
-      <input 
-      name="receiverAddress"
-      matInput 
-      type="text"
-      placeholder="Public Key de la wallet del destinatario."
-      [(ngModel)]="model.receiverAddress"
-      required
-      #receiverAddressControl="ngModel"
-      />
-      <mat-icon matSuffix>key</mat-icon>
-
-      @if(form.submitted && receiverAddressControl.errors){
-        <mat-error>
-        @if(receiverAddressControl.errors['required']) {
-          El monto es obligatorio.
+              <mat-hint>Debe ser el motivo de la transferencia</mat-hint>
           }
-        </mat-error>
+      </mat-form-field>
+
+     
+      <mat-form-field appearance="fill" class="w-full mb-4">
+          <mat-label>Monto</mat-label>
+          <input name="amount" matInput type="number" min="0" placeholder="Ingrese el monto aquí."
+            [(ngModel)]="model.amount" required #amountControl="ngModel">
+          <mat-icon matSuffix>attach_money</mat-icon>
+
+          @if(form.submitted && amountControl.errors){
+              <mat-error>
+              @if(amountControl.errors['required']) {
+                El monto es obligatorio.
+                } @else if (amountControl.errors['min']) {
+                  El monto debe ser mayor a cero.
+                }
+              </mat-error>
           }@else{
-          <mat-hint>El destinatario es obligatorio</mat-hint>
-      }
-      <mat-hint>Debe ser una wallet de Solana.</mat-hint>
-  </mat-form-field>
+              <mat-hint>Debe ser el motivo de la transferencia</mat-hint>
+          }
+      </mat-form-field>
+
+
+      <mat-form-field appearance="fill" class="w-full mb-4">
+          <mat-label>Destinatario</mat-label>
+          <input name="receiverAddress" matInput type="text" placeholder="Public Key de la wallet del destinatario."
+            [(ngModel)]="model.receiverAddress" required #receiverAddressControl="ngModel">
+          <mat-icon matSuffix>key</mat-icon>
+
+          @if(form.submitted && receiverAddressControl.errors){
+              <mat-error>
+              @if(receiverAddressControl.errors['required']) {
+                El monto es obligatorio.
+                }
+              </mat-error>
+          }@else{
+              <mat-hint>El destinatario es obligatorio</mat-hint>
+          }
+      </mat-form-field>
 
     <footer class="flex justify-center">
       <button type="submit" mat-raised-button color="primary">Enviar</button>
