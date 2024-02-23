74b2789259e581d2e93170f4069bc42bc19a87f4
Brenda <brentorres26@gmail.com>
2024-02-23 08:49:26 -0300

    se implementa modal y servicio para mostarr el formulario y procesar transferencia

--- src/app/transfer.modal.component.ts
+++ src/app/transfer.modal.component.ts
@@ -1,4 +1,6 @@
 import { Component } from "@angular/core";
+import { createTransferInstructions } from "@heavy-duty/spl-utils";
+import { injectTransactionSender } from "@heavy-duty/wallet-adapter";
 import { TransferFormComponent, TransferFormPayLoad } from "./transfer-form.component";
 
 
@@ -9,14 +11,33 @@ import { TransferFormComponent, TransferFormPayLoad } from "./transfer-form.comp
     <div class="px-8 pt-16 pb8-">
       <h2 class="text-3xl text-center mb-8">Transferir Fondos</h2>
 
-      <my-proyect-transfer-form (submitForm)="onTransfer($event)"></my-proyect-transfer-form>
+      <my-proyect-transfer-form (submitForm)="onTransfer($event)"></my-proyect-transfer-form> <!--$event-->
     </div>
     `,
     imports: [TransferModalComponent, TransferFormComponent]
 })
 
   export class TransferModalComponent {
+    private readonly _transactionSender = injectTransactionSender();
     onTransfer(payload: TransferFormPayLoad){
-      console.log('Hola mundo', payload);
+  
+      this._transactionSender.send(({publicKey})=> 
+        createTransferInstructions({
+          amount: payload.amount,
+          mintAddress: '7EYnhQoR9YM3N7UoaKRoA44Uy8JeaZV3qyouov87awMs',
+          receiverAddress: payload.receiverAddress,
+          senderAddress: publicKey.toBase58(),
+          fundReceiver: true,
+          memo: payload.memo
+        }),
+
+      ).subscribe({
+        next:(signature)=>console.log(`Firma: ${signature}`),
+        error: (error) => console.error(error),
+        complete: () => console.log('Transaccion lista.')
+      })
     }
+
   }
+
+
