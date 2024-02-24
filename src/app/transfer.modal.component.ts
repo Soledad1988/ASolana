1ce023c0c5a0393e65c55d0ab50608f9b5b98566
Brenda <brentorres26@gmail.com>
2024-02-23 21:55:34 -0300

    correción para unificar visualización de montos (multiplicar el amount)

--- src/app/transfer.modal.component.ts
+++ src/app/transfer.modal.component.ts
@@ -31,7 +31,7 @@ import { TransferFormComponent, TransferFormPayLoad } from "./transfer-form.comp
     onTransfer(payload: TransferFormPayLoad){
       this._transactionSender.send(({publicKey})=> 
         createTransferInstructions({
-          amount: payload.amount,
+          amount: payload.amount*10**9,
           mintAddress: '7EYnhQoR9YM3N7UoaKRoA44Uy8JeaZV3qyouov87awMs',
           receiverAddress: payload.receiverAddress,
           senderAddress: publicKey.toBase58(),
