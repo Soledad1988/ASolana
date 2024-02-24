db3b937c3b99d585cf1b19cf0be09a752c2aa975
Brenda <brentorres26@gmail.com>
2024-02-24 18:49:32 -0300

    se establecieron colores

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        myGray:'#666666',
+        surgeGreen: '#00FFA3',
+        PurpleDino: '#DC1FFF',
+        OceanBlue: '#03E1FF'
+      }
+    },
   },
   plugins: [],
 }
