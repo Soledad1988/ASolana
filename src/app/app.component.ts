c728aa06c93862a33f456dcbc4591ad76af9c975
Brenda <brentorres26@gmail.com>
2024-02-29 19:34:27 -0300

    Adaptar la imagen al componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { DecimalPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { MatAnchor } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
+import { WalletStore } from '@heavy-duty/wallet-adapter';
 import { HdWalletMultiButtonComponent } from '@heavy-duty/wallet-adapter-material';
+import { computedAsync } from 'ngxtension/computed-async';
 import { FeaturesectionComponent } from "./features-section.component";
 import { HeroSectionComponent } from "./hero-section.component";
+import { ShyftApiService } from './shyft-api-service';
 
 @Component({
     standalone: true,
@@ -13,6 +17,15 @@ import { HeroSectionComponent } from "./hero-section.component";
    <div class="flex justify-center items-center h-screen">
       <div class="w-full max-w-2xl sm:max-w-3xl rounded-lg overflow-hidden">
         <header class="px-16 pt-18 pb-8 relative bg-black text-white">
+
+          <div class="flex justify-between items-center mb-4">
+          @if (account()) {
+            <div class ="flex justify-center items-center gap-1 mt-4">
+              <img src="./assets/img/solana.png" class="w-12 h-12" />
+              <p class="text-sm">{{ account()?.balance }}</p>
+            </div>
+            }
+          </div>
 
           <div class="flex justify-between items-center mb-4">
           <div></div><!-- Espacio en blanco para alinear el icono de configuración -->
@@ -63,6 +76,15 @@ import { HeroSectionComponent } from "./hero-section.component";
 })
 
 export class AppComponent {
+  private readonly _shyftApiService = inject(ShyftApiService);
+  private readonly _walletStore = inject(WalletStore);
+  private readonly _publicKey = toSignal(this._walletStore.publicKey$);
+  
+  readonly account = computedAsync(
+    () => this._shyftApiService.getAccount(this._publicKey()?.toBase58()),
+    {requireSync: false},
+  );
+
 }
 
 
